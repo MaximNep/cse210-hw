7c512c3cefc028516d971ad7bba3e4119c3bf8c4
MaximNep <122553456+MaximNep@users.noreply.github.com>
2023-07-04 02:19:42 +0300

    Create Journal.cs

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace JournalApp
+{
+   
+    class Journal
+    {
+        private List<Entry> entries;
+
+        public Journal()
+        {
+            entries = new List<Entry>();
+        }
+
+        public void AddEntry(string prompt, string response, string date)
+        {
+            Entry entry = new Entry
+            {
+                Prompt = prompt,
+                Response = response,
+                Date = date
+            };
+
+            entries.Add(entry);
+        }
+
+        public void DisplayEntries()
+        {
+            foreach (Entry entry in entries)
+            {
+                Console.WriteLine($"Date: {entry.Date}\nPrompt: {entry.Prompt}\nResponse: {entry.Response}\n");
+            }
+        }
+
+        public void SaveToFile(string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (Entry entry in entries)
+                {
+                    writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+                }
+            }
+        }
+
+        public void LoadFromFile(string filename)
+        {
+            entries.Clear();
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split('|');
+                    if (parts.Length == 3)
+                    {
+                        Entry entry = new Entry
+                        {
+                            Date = parts[0],
+                            Prompt = parts[1],
+                            Response = parts[2]
+                        };
+
+                        entries.Add(entry);
+                    }
+                }
+            }
+        }
+    }
+}
