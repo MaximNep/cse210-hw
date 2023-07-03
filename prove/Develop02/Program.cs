108b2fe05c19f071078e92cddc25216e0989ec85
MaximNep <122553456+MaximNep@users.noreply.github.com>
2023-07-04 02:20:24 +0300

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,65 @@
 using System;
 
-class Program
+namespace JournalApp
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
-    }
-}
+        private static Journal journal;
+
+        static void Main(string[] args)
+        {
+            journal = new Journal();
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Journal App Menu:");
+                Console.WriteLine("1. Write a new entry");
+                Console.WriteLine("2. Display the journal");
+                Console.WriteLine("3. Save the journal to a file");
+                Console.WriteLine("4. Load the journal from a file");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            WriteNewEntry();
+                            break;
+                        case 2:
+                            DisplayJournal();
+                            break;
+                        case 3:
+                            SaveJournalToFile();
+                            break;
+                        case 4:
+                            LoadJournalFromFile();
+                            break;
+                        case 5:
+                            exit = true;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void WriteNewEntry()
+        {
+            string[] prompts = {
+                "Who was the most interesting person I interacted with today?",
+                "What was the best part of my day?",
+                "How did I see the hand of the Lord in my life today?",
+                "What was the strongest emotion I felt today?",
+                "
