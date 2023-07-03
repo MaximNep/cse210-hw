22801de72ce27a8b6ab06811cd574712ca90e1ca
MaximNep <122553456+MaximNep@users.noreply.github.com>
2023-07-04 02:46:09 +0300

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,5 +6,17 @@ namespace ScriptureMemoryApp
     {
         static void Main(string[] args)
         {
-            string reference = "John 6:26";
-            string text = "Jesus answered them and said, Verily
+            Scripture scripture = new Scripture("John 6:26, Matthew 8:12");
+            scripture.AddVerse(new Verse("John 6:26", "Jesus answered them and said, Verily, verily, I say unto you, Ye seek me, not because ye saw the miracles, but because ye did eat of the loaves, and were filled."));
+            scripture.AddVerse(new Verse("Matthew 8:12", "But the children of the kingdom shall be cast out into outer darkness: there shall be weeping and gnashing of teeth."));
+
+            while (true)
+            {
+                Console.Clear();
+                scripture.Display();
+
+                if (scripture.AllWordsHidden())
+                {
+                    Console.WriteLine("All words hidden. Press any key to exit.");
+                    Console.ReadKey();
+                   
