1ed7780f1015511cb366cf9cac53e53f3b04048c
MaximNep <122553456+MaximNep@users.noreply.github.com>
2023-07-03 14:51:24 +0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Courier";
+        job1._company = "Yandex";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Courier";
+        job2._company = "Samokat";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Maxim Nepomnyaschy";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
