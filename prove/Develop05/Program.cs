b29dc32d7a41c1ccd9bee7a1229bf1a4c42d1845
MaximNep <122553456+MaximNep@users.noreply.github.com>
2023-07-04 03:25:02 +0300

    Update Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        QuestProgram questProgram = new QuestProgram();
+
+        
+        Goal simpleGoal = new SimpleGoal("Run a marathon", 1000);
+        Goal eternalGoal = new EternalGoal("Read scriptures", 100);
+        Goal checklistGoal = new ChecklistGoal("Attend the temple", 50, 10);
+
+        
+        questProgram.AddGoal(simpleGoal);
+        questProgram.AddGoal(eternalGoal);
+        questProgram.AddGoal(checklistGoal);
+
+        
+        questProgram.RecordEvent("Run a marathon");
+        questProgram.RecordEvent("Read scriptures");
+        questProgram.RecordEvent("Attend the temple");
+        questProgram.RecordEvent("Attend the temple");
+        questProgram.RecordEvent("Attend the temple");
+
+        
+        questProgram.DisplayScore();
+        questProgram.DisplayGoals();
+
+        
+        questProgram.SaveGoals();
+        questProgram.LoadGoals();
+
+        Console.ReadKey();
     }
 }
