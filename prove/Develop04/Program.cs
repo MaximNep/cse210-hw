565cebf74adebbcbb16ad600e9b79147edc8287d
MaximNep <122553456+MaximNep@users.noreply.github.com>
2023-07-04 03:19:29 +0300

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,56 @@
 using System;
 
-class Program
+namespace MindfulnessApp
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop04 World!");
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("=====================================");
+                Console.WriteLine("Mindfulness App");
+                Console.WriteLine("=====================================");
+                Console.WriteLine("1. Breathing Activity");
+                Console.WriteLine("2. Reflection Activity");
+                Console.WriteLine("3. Listing Activity");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("=====================================");
+                Console.Write("Enter your choice (1-4): ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        BreathingActivity breathingActivity = new BreathingActivity();
+                        breathingActivity.Start();
+                        break;
+                    case "2":
+                        ReflectionActivity reflectionActivity = new ReflectionActivity();
+                        reflectionActivity.Start();
+                        break;
+                    case "3":
+                        ListingActivity listingActivity = new ListingActivity();
+                        listingActivity.Start();
+                        break;
+                    case "4":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number from 1 to 4.");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("=====================================");
+                Console.WriteLine("Press any key to continue...");
+                Console.WriteLine("=====================================");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
     }
 }
